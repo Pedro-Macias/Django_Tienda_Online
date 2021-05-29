8b0056f47ae261ac92b2e150d73b650fb4d2d566
Pedro Macias <pin.macias@gmail.com>
2021-05-29 12:57:26 +0200

    formulario de pago

--- carro/admin.py
+++ carro/admin.py
@@ -1,9 +1,20 @@
 from django.contrib import admin
-from .models import Product, Order, OrderItem, TipoQueso
+from .models import Product, Order, OrderItem, TipoQueso, Address
 # Register your models here.
+
+class AddressAdmin(admin.ModelAdmin):
+    list_display=[
+        'address_line_1',
+        'address_line_2',
+        'zip_code',
+        'city',
+        'address_type',
+    ]
+
 
 # registramos el modelo producto en el administrador
 admin.site.register(Product)
 admin.site.register(Order)
 admin.site.register(OrderItem)
 admin.site.register(TipoQueso)
+admin.site.register(Address, AddressAdmin)
--- carro/forms.py
+++ carro/forms.py
@@ -1,6 +1,8 @@
 from django import forms
-from .models import OrderItem,TipoQueso, Product
+from .models import Address, OrderItem,TipoQueso, Product
+from django.contrib.auth import get_user_model 
 
+User = get_user_model()
 
 class AddToCarroForm(forms.ModelForm):
     tipo = forms.ModelChoiceField(queryset=TipoQueso.objects.none())
@@ -17,4 +19,64 @@ class AddToCarroForm(forms.ModelForm):
 
 # formulario de direccion
 class AddressForm(forms.Form):
-    pass
+    shipping_address_line_1 = forms.CharField(required=False)
+    shipping_address_line_2 = forms.CharField(required=False)
+    shipping_zip_code = forms.CharField(required=False)
+    shipping_city = forms.CharField(required=False)
+
+    billing_address_line_1 = forms.CharField(required=False)
+    billing_address_line_2 = forms.CharField(required=False)
+    billing_zip_code = forms.CharField(required=False)
+    billing_city = forms.CharField(required=False)
+    
+    selected_shipping_address = forms.ModelChoiceField(
+        Address.objects.none(),required=False
+    )
+    selected_billing_address = forms.ModelChoiceField(
+        Address.objects.none(),required=False
+    )
+
+    def __init__(self, *args, **kwargs):
+        user_id = kwargs.pop('user_id')
+        super().__init__(*args, **kwargs)
+        
+        user = User.objects.get(id = user_id)
+
+
+        shipping_address_qs = Address.objects.filter(
+            user= user,
+            address_type = 'S'
+            )
+
+        billing_address_qs = Address.objects.filter(
+            user= user,
+            address_type = 'B'
+            )
+        
+        self.fields['selected_shipping_address'].queryset = shipping_address_qs
+        self.fields['selected_billing_address'].queryset = billing_address_qs
+
+    def clean(self):
+        data = self.cleaned_data 
+
+        selected_shipping_address = data.get('selected_shipping_address', None)
+        if selected_shipping_address is None:
+            if not data.get('shipping_address_line_1', None):
+                self.add_error('shipping_address_line_1','Por favor complete este campo')
+            if not data.get('shipping_address_line_2',None):
+                self.add_error('shipping_address_line_2','Por favor complete este campo')
+            if not data.get('shipping_zip_code',None):
+                self.add_error('shipping_zip_code','Por favor complete este campo')
+            if not data.get('shipping_city',None):
+                self.add_error('shipping_city','Por favor complete este campo')
+
+        selected_billing_address = data.get('selected_billing_address', None)
+        if selected_billing_address is None:
+            if not data.get('billing_address_line_1', None):
+                self.add_error('billing_address_line_1','Por favor complete este campo')
+            if not data.get('billing_address_line_2',None):
+                self.add_error('billing_address_line_2','Por favor complete este campo')
+            if not data.get('billing_zip_code',None):
+                self.add_error('billing_zip_code','Por favor complete este campo')
+            if not data.get('billing_city',None):
+                self.add_error('billing_city','Por favor complete este campo')
